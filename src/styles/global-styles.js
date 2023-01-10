f4cf252abf8ae6648bd8f98945a9c3a49232c4fa
CleytonLu <cleytonlucasiurd@gmail.com>
2023-01-10 11:25:18 -0300

    Configurando fonts

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,14 +1,24 @@
 import {createGlobalStyle, css} from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+
+@import url('https://fonts.googleapis.com/css2?family= Montserrat: família= Montserrat:ital,wght@0,900;1,900 &família= Open+Sans:ital,wght@0,400;0,700;1,400;1,700 & display=swap');
+
     *{
         margin: 0;
         padding: 0;
     }
 
+    html{
+        font-size: 62.5%;
+    }
+
     body{
-        ${( { theme } ) => css `
-            background: ${theme.colors.mainBg}
-        `}
+        font-size: 1.6rem;
+        font-family: ${({ theme }) => theme.fonts.family.default};
+    }
+
+    h1{
+        font-family: ${({ theme }) => theme.fonts.family.secondary};
     }
 `
