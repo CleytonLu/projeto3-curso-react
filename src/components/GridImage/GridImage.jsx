013f84c8db6d57b5827b30d9e78abb884ae5a343
cleyton.cabral <cleyton.cabral@ferreiracosta.com.br>
2023-02-22 15:45:26 -0300

    GridImage

---
+++ src/components/GridImage/GridImage.jsx
@@ -0,0 +1,38 @@
+import * as Styled from './styles';
+import P, { shape } from 'prop-types';
+
+import {SectionBackground} from '../SectionBackground/SectionBackground'
+import {Heading} from '../Heading/Heading'
+import {TextComponent} from '../TextComponent/TextComponent'
+
+export const GridImage = ({ title, description, grid, background }) => {
+    return (
+        <SectionBackground background={background}>
+            <Styled.Container>
+                <Heading size="huge" uppercase colorDark={!background}>
+                    {title}
+                </Heading>
+                <TextComponent>{description}</TextComponent>
+                <Styled.Grid>
+                    {grid.map((item) => (
+                        <Styled.GridElement key={item.srcImg}>
+                            <Styled.Image src={item.srcImg} alt={item.altText} />
+                        </Styled.GridElement>
+                    ))}
+                </Styled.Grid>
+            </Styled.Container>
+        </SectionBackground>
+    );
+};
+
+GridImage.propTypes = {
+    background: P.bool,
+    title: P.string.isRequired,
+    description: P.string.isRequired,
+    grid: P.arrayOf(
+        P.shape({
+            altText: P.string.isRequired,
+            srcImg: P.string.isRequired,
+        })
+    ),
+};
---
+++ src/components/GridImage/styles.js
@@ -0,0 +1,50 @@
+import styled, { css } from 'styled-components';
+import { Title as HeadingContainer } from '../Heading/styles';
+import { Container as TextContainer } from '../TextComponent/styles';
+
+export const Container = styled.div`
+    ${({ theme, background }) => css`
+        ${TextContainer}{
+            margin-bottom: ${theme.spacings.huge};
+        }
+    `}
+`;
+
+export const Grid = styled.div`
+    ${({ theme }) => css`
+        counter-reset: grid-counter;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+        gap: ${theme.spacings.large};
+    `}
+`;
+
+export const GridElement = styled.div`
+    ${({ theme }) => css`
+        ${HeadingContainer}{
+            position: relative;
+            left: 5rem;
+        }
+
+        ${HeadingContainer}::before{
+            counter-increment: grid-counter;
+            content: counter(grid-counter);
+            position: absolute;
+            font-size: 8rem;
+            top: -3rem;
+            left: -5rem;
+            transform: rotate(6deg);
+        }
+    `}
+`;
+
+export const Image = styled.img`
+    ${({ theme }) => css`
+        width: 100%;
+        transition: all 300ms ease-in-out;
+
+        &:hover{
+            transform: scale(1) rotate(10deg);
+        }
+    `}
+`;
