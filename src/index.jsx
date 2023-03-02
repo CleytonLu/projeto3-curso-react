cb3ad06fe5487093fc233578e7658af10b279e64
cleyton.cabral <cleyton.cabral@ferreiracosta.com.br>
2023-03-02 11:11:19 -0300

    Atualizações do funcionamento da API com o Front end

--- src/index.jsx
+++ src/index.jsx
@@ -2,44 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { ThemeProvider } from 'styled-components';
-import {theme} from './styles/theme'
+import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global-styles';
-import Home from './templates/App/Home';
+import Home from './templates/Home/Home';
 
-import styled, {css} from 'styled-components'
+import styled, { css } from 'styled-components';
 
 const Heading = styled.h1`
     color: purple;
 `;
 
-const changeBackground = (theme, bg) => css `
+const changeBackground = (theme, bg) => css`
     background: ${bg};
     color: #000;
-`
+`;
 
 export const Container = styled.div`
-    ${({theme, bg}) => css `
-        color: ${theme.colors.white}
-        ${changeBackground(theme, bg)}
+    ${({ theme, bg }) => css`
+        color: ${theme.colors.white} ${changeBackground(theme, bg)};
     `}
 
-    ${Heading}{
+    ${Heading} {
         color: yellow;
     }
 `;
 
-export const Container2 = styled(Container).attrs({as: 'article'})`
+export const Container2 = styled(Container).attrs({ as: 'article' })`
     background: blue;
 `;
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
             <Home />
-        <GlobalStyles />
-    </ThemeProvider>
-  </React.StrictMode>
+            <GlobalStyles />
+        </ThemeProvider>
+    </React.StrictMode>
 );
