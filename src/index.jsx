e63bde0b6dd6db4cf4a0d600f92ef59b6d9b04c3
cleyton.cabral <cleyton.cabral@ferreiracosta.com.br>
2023-03-02 18:50:25 -0300

    Ajustes no sectionBackground e instalação do react router dom

--- src/index.jsx
+++ src/index.jsx
@@ -4,38 +4,21 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global-styles';
+
 import Home from './templates/Home/Home';
 
-import styled, { css } from 'styled-components';
-
-const Heading = styled.h1`
-    color: purple;
-`;
-
-const changeBackground = (theme, bg) => css`
-    background: ${bg};
-    color: #000;
-`;
-
-export const Container = styled.div`
-    ${({ theme, bg }) => css`
-        color: ${theme.colors.white} ${changeBackground(theme, bg)};
-    `}
-
-    ${Heading} {
-        color: yellow;
-    }
-`;
-
-export const Container2 = styled(Container).attrs({ as: 'article' })`
-    background: blue;
-`;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="*" element={<Home />} />
+                </Routes>
+            </BrowserRouter>
+
             <GlobalStyles />
         </ThemeProvider>
     </React.StrictMode>
